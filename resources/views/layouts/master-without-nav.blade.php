f057d3096d513dce1ee8e77df6da368fe043a610
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-07-23 09:13:49 +0530

    update theme and login page

--- resources/views/layouts/master-without-nav.blade.php
+++ resources/views/layouts/master-without-nav.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-topbar="light" data-body-image="img-1" data-sidebar-image="none" data-layout-mode="dark">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-topbar="light" data-body-image="img-1" data-sidebar-image="none" data-layout-mode="light">
 
     <head>
     <meta charset="utf-8" />
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,5 +1,5 @@
 <!doctype html >
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-layout-mode="dark" data-body-image="img-1" data-preloader="disable">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-layout-mode="light" data-body-image="img-1" data-preloader="disable">
 
 <head>
     <meta charset="utf-8" />
