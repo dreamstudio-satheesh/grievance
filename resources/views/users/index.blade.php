befe08116111ba1f70d5ab0a5e610d51fd775ef3
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-07-24 19:07:13 +0530

    add user menu

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+@section('title') @lang('translation.dashboards') @endsection
+@section('content')
+@component('components.breadcrumb')
+@slot('li_1') Pages @endslot
+@slot('title') Users  @endslot
+@endcomponent
+@endsection
+@section('script')
+<script src="{{ URL::asset('/assets/js/app.min.js') }}"></script>
+@endsection
