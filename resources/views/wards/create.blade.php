f9ecfc1dda09dd942cf4b1b7ef8ea09f611b5946
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-07-25 18:19:23 +0530

    add ward

--- resources/views/wards/create.blade.php
+++ resources/views/wards/create.blade.php
@@ -16,6 +16,7 @@
             <div class="mb-3">
                 <label for="customername-field" class="form-label">Ward Name</label>
                 <input type="text" name="name" class="form-control" placeholder="Enter Name" required="">
+                <input type="submit" value="submit" />
                 <div class="invalid-feedback">Please enter ward name.</div>
             </div>
 
@@ -23,7 +24,7 @@
         <div class="card-footer" >
             <div class="hstack gap-2 justify-content-end">                
                 {{-- <button type="submit" value="submit" class="btn btn-success">Add Ward</button> --}}
-                <input type="submit" value="submit" />
+                
             </div>
         </div>
     </form>
