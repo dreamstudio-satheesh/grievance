2e4df9af200d47b169dd84137ad24d54c2dd5fae
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-07-26 10:08:43 +0530

    add street

--- resources/views/streets/create.blade.php
+++ resources/views/streets/create.blade.php
@@ -3,49 +3,27 @@
 @section('content')
 @component('components.breadcrumb')
 @slot('li_1') Pages @endslot
-@slot('title') Add Wards  @endslot
+@slot('title') Add street  @endslot
 @endcomponent
 
 
     <div class="card">
-    <form action="{{ route('wards.store') }}"  method="post" >
+    <form action="{{ route('streets.store') }}"  method="post" >
         @csrf
         <div class="card-body">
             
 
             <div class="mb-3">
-                <label for="customername-field" class="form-label">Ward Name</label>
+                <label for="customername-field" class="form-label">street Name</label>
                 <input type="text" name="name"  class="form-control" placeholder="Enter Name" required="">
-                <div class="invalid-feedback">Please enter ward name.</div>
-            </div>
-            <div class="mb-3">
-                <label for="customername-field" class="form-label">JE Name</label>
-                <input type="text" name="jename"  class="form-control" placeholder="Enter JE Name" required="">
-                <div class="invalid-feedback">Please enter JE name.</div>
+                <div class="invalid-feedback">Please enter street name.</div>
             </div>
            
-            <div class="mb-3">
-                <label for="customername-field" class="form-label">JE Mobile</label>
-                <input type="text" name="jemobile"  class="form-control" placeholder="Enter JE Mobile" required="">
-                <div class="invalid-feedback">Please enter JE Mobile.</div>
-            </div>
-
-            <div class="mb-3">
-                <label for="customername-field" class="form-label">AE Name</label>
-                <input type="text" name="aename"  class="form-control" placeholder="Enter AE Name" required="">
-                <div class="invalid-feedback">Please enter AE name.</div>
-            </div>
-            <div class="mb-3">
-                <label for="customername-field" class="form-label">AE Mobile</label>
-                <input type="text" name="aemobile"  class="form-control" placeholder="Enter AE Mobile" required="">
-                <div class="invalid-feedback">Please enter AE Mobile.</div>
-            </div>
-
         </div>
         <div class="card-footer" >
             <div class="hstack gap-2 justify-content-end">
                 
-                <button type="submit" class="btn btn-success"> Add Ward</button> 
+                <button type="submit" class="btn btn-success"> Add street</button> 
             </form>
             </div>
         </div>
