da314d63ca5206e62ee95c56a4a2dc20e0d6ae79
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-07-27 17:05:31 +0530

    update

--- database/migrations/2023_07_27_112103_create_dstreets_table.php
+++ database/migrations/2023_07_27_112103_create_dstreets_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
     {
         Schema::create('dstreets', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->string('name');
             $table->unsignedBigInteger('division_id');
             $table->timestamps();
