0ea3d8598e4dd7de2bee8da2e78742fb3114e901
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-07-28 09:28:04 +0530

    update complaints

--- resources/views/complaints/create.blade.php
+++ resources/views/complaints/create.blade.php
@@ -8,18 +8,84 @@
 
 
     <div class="card">
-    <form action="{{ route('complaints.store') }}"  method="post" >
+        <form action="{{ route('complaints.store') }}"  method="post" >
         @csrf
         <div class="card-body">
 
+            
+
             @livewire('show-city-town')
-            
 
-           
-            
-           
+            <div class="mb-3">
+                <label class="form-label text-muted">Grievance Type</label>
+                <select name="" class="form-control" required >
+                <option selected="" value="">Select</option>
+                <option value="Blockage of drainage">Blockage of drainage</option>
+                <option value="Mosquito menace">Mosquito menace</option>
+                <option value="Street Dog menace">Street Dog menace</option>
+                <option value="Stagnant of water on the road">Stagnant of water on the road</option>
+                <option value="Pig menace">Pig menace</option>
+                <option value="Mixing of waste water in drinking water">Mixing of waste water in drinking water</option>
+                <option value="Dust bin removal           ">Dust bin removal           </option>
+                <option value="Public Health">Public Health</option>
+                <option value="Unsanitary conditions on the road       ">Unsanitary conditions on the road       </option>
+                <option value="Complaints regarding dust, smoke ">Complaints regarding dust, smoke </option>
+                <option value="Damage against public property  ">Damage against public property  </option>
+                <option value="New street light   ">New street light   </option>
+                <option value="Non burning of street lights  ">Non burning of street lights  </option>
+                <option value="Complaint regarding toilet   ">Complaint regarding toilet   </option>
+                <option value="Improper maintenance of  toilet   ">Improper maintenance of  toilet   </option>
+                <option value="Burning of street light during day time   ">Burning of street light during day time   </option>
+                <option value="Mixing of waste water in drinking water   ">Mixing of waste water in drinking water   </option>
+                <option value="Broken of water tank       ">Broken of water tank       </option>
+                <option value="Sales of water   ">Sales of water   </option>
+                <option value="No water supply                   ">No water supply                   </option>
+                <option value="Reqest to lay new drainage line     ">Reqest to lay new drainage line     </option>
+                <option value="Request to rely the road      ">Request to rely the road      </option>
+                <option value="No speed breake  ">No speed breake  </option>
+                <option value="Road damage     ">Road damage     </option>
+                <option value="Stagnant of water on the road      ">Stagnant of water on the road      </option>
+                <option value="Encroachment of sand in public road   ">Encroachment of sand in public road   </option>
+                <option value="Complaint regarding street name board ">Complaint regarding street name board </option>
+                <option value="Encroachment">Encroachment</option>
+                <option value="Encroachment of rocks in public road ">Encroachment of rocks in public road </option>
+                <option value="Water tank ">Water tank </option>
+                <option value="Request to swimming pool    ">Request to swimming pool    </option>
+                <option value="Delay of pension    ">Delay of pension    </option>
+                <option value="Delay in changing the name in certificates ">Delay in changing the name in certificates </option>
+                <option value="Tax collection ">Tax collection </option>
+                <option value="Other Complaints">Other Complaints</option>  
+                </select>
+            </div>
 
+            <div class="mb-3">
+                <label for="customername-field" class="form-label">Desctiption</label>
+                <textarea name="description"  class="form-control" required="" cols="30" rows="10"></textarea>
+            </div>
+
+
+            <div class="mb-3">
+                <label class="form-label text-muted">*Priority</label>
+                <select name="priority" class="form-control" required >
+                <option selected="" value="Medium">Medium</option>
+                <option value="High">High</option>
+                <option value="Low">Low</option>
+                 
+                </select>
+            </div>
+
+            <div class="mb-3">
+                <label for="customername-field" class="form-label"> Name</label>
+                <input type="text" name="name"  class="form-control" placeholder="Enter Name" required="">
+            </div>
+
+
+
+   
         </div>
+    
+           
+
         <div class="card-footer" >
             <div class="hstack gap-2 justify-content-end">
                 
--- resources/views/livewire/show-city-town.blade.php
+++ resources/views/livewire/show-city-town.blade.php
@@ -3,7 +3,7 @@
 
     <div class="mb-3">
         <label class="form-label text-muted">Select Type</label>
-        <select  wire:model="Selectedtype"  class="form-control" >
+        <select  wire:model="Selectedtype"  class="form-control" required >
             <option selected="" value="Select">Select</option>
             <option value="City">City</option>
             <option value="Town">Town panchayat</option>            
@@ -14,7 +14,7 @@
 
             <div class="mb-3">
                 <label class="form-label text-muted">Select Divisions</label>
-                <select name="division_id"  wire:model="SelectedDivision"  class="form-control" >
+                <select name="division_id"  wire:model="SelectedDivision"  class="form-control"  required>
                     <option selected=""  value="">Select</option>
                     @foreach ($divisions as $division)
                     <option value="{{ $division->id }}">{{ $division->name }}</option>
@@ -26,7 +26,7 @@
 
             <div class="mb-3">                
                 <label class="form-label text-muted">Select Street</label>
-                <select name="dsreet_id"   class="form-control" >
+                <select name="dsreet_id"   class="form-control" required >
                     <option selected=""  value="">Select</option>
                     @foreach ($dstreets as $dstreet)
                     <option value="{{ $dstreet->id }}">{{ $dstreet->name }}</option>
@@ -41,7 +41,7 @@
 
         <div class="mb-3">
             <label class="form-label text-muted">Select Panchayat</label>
-            <select name="panchayat_id"  wire:model="SelectedPanchayat"  class="form-control" >
+            <select name="panchayat_id"  wire:model="SelectedPanchayat"  class="form-control" required >
                 <option selected=""  value="">Select</option>
                 @foreach ($panchayats as $panchayat)
                 <option value="{{ $panchayat->id }}">{{ $panchayat->name }}</option>
@@ -53,7 +53,7 @@
 
         <div class="mb-3">                
             <label class="form-label text-muted">Select Ward</label>
-            <select name="ward_id" wire:model="SelectedWard"  class="form-control" >
+            <select name="ward_id" wire:model="SelectedWard"  class="form-control"  required>
                 <option selected=""  value="">Select Ward</option>
                 @foreach ($wards as $ward)
                 <option value="{{ $ward->id }}">{{ $ward->name }}</option>
@@ -65,7 +65,7 @@
         @if (!is_null($streets))
         <div class="mb-3">                
             <label class="form-label text-muted">Select Street</label>
-            <select name="sreet_id"   class="form-control" >
+            <select name="sreet_id"   class="form-control" required >
                 <option selected=""  value="">Select</option>
                 @foreach ($streets as $street)
                 <option value="{{ $street->id }}">{{ $street->name }}</option>
@@ -79,10 +79,5 @@
 
 
 </div>
-
-
-   
-</div>
-
 
 
