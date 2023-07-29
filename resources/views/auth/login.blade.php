71a79de7089a44186c2b2c1dea23495f1d48c061
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-07-29 18:28:55 +0530

    login logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                 <div class="col-lg-12">
                     <div class="text-center mt-sm-5 mb-4 text-white-50">
                         <div>
-                            <a href="index" class="d-inline-block auth-logo">
+                            <a href="{{ url('/') }}" class="d-inline-block auth-logo">
                                 <img src="{{ URL::asset('assets/images/logo/01.png')}}" alt="" height="90">
                             </a>
                         </div>
