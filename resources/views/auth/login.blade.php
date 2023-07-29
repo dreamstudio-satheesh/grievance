5781720fbfeec1717058e1de35566434db4f10a7
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-07-29 18:27:59 +0530

    login logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
                     <div class="text-center mt-sm-5 mb-4 text-white-50">
                         <div>
                             <a href="index" class="d-inline-block auth-logo">
-                                <img src="{{ URL::asset('assets/images/logo/01.png')}}" alt="" height="60">
+                                <img src="{{ URL::asset('assets/images/logo/01.png')}}" alt="" height="90">
                             </a>
                         </div>
                         <p class="mt-3 fs-15 fw-medium">Clean & Green  Salem City</p>
