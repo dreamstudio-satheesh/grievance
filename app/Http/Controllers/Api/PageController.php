3dbadec1c965cd1998738365454ae0bde7ab4dcf
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-08-03 10:12:37 +0530

    add api

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use App\Models\NewsEvent;
+use App\Http\Resources\News;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Api\BaseController as BaseController;
 
@@ -13,12 +14,9 @@ class PageController extends BaseController
 
     public function news_events()
     {
-        $newsevents = NewsEvent::with('media')->get();
+        $newsevents = NewsEvent::with('media')->get();        
 
-        return $newsevents;
-
-       // return $this->sendResponse(BlogResource::collection($blogs), 'Posts fetched.');
-     
+       return $this->sendResponse(News::collection($newsevents), 'Posts fetched.');     
       
     }
 
