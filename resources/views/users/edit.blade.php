5053dfa25e179d12d1dba9a785d6a54091d3f43c
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-08-09 22:47:47 +0530

    edit user

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -3,13 +3,14 @@
 @section('content')
 @component('components.breadcrumb')
 @slot('li_1') Pages @endslot
-@slot('title') Add User  @endslot
+@slot('title') Edit User  @endslot
 @endcomponent
 
 
     <div class="card">
-        <form action="{{ route('users.store') }}"  method="POST" >
+        <form action="{{ route('users.update',$user->id) }}"  method="POST" >
             @csrf
+            @method('PUT')
         <div class="card-body">
 
             @if ($errors->any())
@@ -27,24 +28,24 @@
 
             <div class="mb-3">
                 <label for="customername-field" class="form-label">User Name</label>
-                <input type="text" name="name" id="customername-field" class="form-control" placeholder="Enter Name" required="">
+                <input type="text" name="name" id="customername-field" class="form-control" value="{{ $user->name }}"required="">
                 <div class="invalid-feedback">Please enter user name.</div>
             </div>
 
             <div class="mb-3">
                 <label for="email-field" class="form-label">Email</label>
-                <input type="email" name="email" id="email-field" class="form-control" placeholder="Enter Email" required="">
+                <input type="email" name="email" id="email-field" class="form-control"value="{{ $user->email }}" required="">
                 <div class="invalid-feedback">Please enter an email.</div>
             
             <div class="mb-3">
                 <label for="phone-field" class="form-label">Phone</label>
-                <input type="text" name="mobile_number" id="phone-field" class="form-control" placeholder="Enter Phone no." required="">
+                <input type="text" name="mobile_number" id="phone-field" class="form-control" value="{{ $user->mobile_number }}" required="">
                 <div class="invalid-feedback">Please enter a phone.</div>
             </div>
 
             <div class="mb-3">
                 <label for="phone-field" class="form-label">Password</label>
-                <input type="text" name="password" id="password" class="form-control" placeholder="Enter password" required="">
+                <input type="text" name="password" id="password" class="form-control" value="{{ $user->password }}" required="">
                 <div class="invalid-feedback">Please enter password.</div>
             </div>
 
