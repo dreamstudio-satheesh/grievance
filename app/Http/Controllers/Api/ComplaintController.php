d170e36fb5a0a41a550f147139223eeec35d61c7
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-08-09 11:50:20 +0530

    add api

--- app/Http/Controllers/Api/ComplaintController.php
+++ app/Http/Controllers/Api/ComplaintController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 
 class ComplaintController extends Controller
 {
@@ -12,6 +13,27 @@ class ComplaintController extends Controller
     public function register(Request $request)
     {
         if ($request->user()) { 
+
+            $validator = Validator::make($request->all(), [              
+                'complaint_id' => 'required|unique:complaints',
+                'username' => 'required|string|max:255',
+                'mobile' => 'required|string|max:11',
+                'email' => 'required|email',
+                'doorno' => 'required|string',
+                'panchayat_id' => 'nullable|integer',
+                'ward_id' => 'nullable|integer',
+                'street_id' => 'nullable|integer',
+                'division_id' => 'nullable|integer',
+                'dstreet_id' => 'nullable|integer',
+                'subject' => 'required|string|max:255',
+                'priority' => 'required|string',
+                'description' => 'required|string',
+                'status' => 'required|string|in:new,in-progress,resolved',
+            ]);
+
+            if($validator->fails()){
+                return $this->sendError('Error validation', $validator->errors());       
+            }
           
             return $request->user()->id;
             return response()->json([
