b647a953ac292beb23be08e366763383e2e0c77c
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-08-09 11:35:04 +0530

    add api

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PageController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ComplaintController;
 
 /*
 |--------------------------------------------------------------------------
