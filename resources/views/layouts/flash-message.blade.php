54fa989a499cff6166acd598aabebe895e5a1fe0
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-08-10 00:04:38 +0530

    flash message

--- resources/views/layouts/flash-message.blade.php
+++ resources/views/layouts/flash-message.blade.php
@@ -5,7 +5,7 @@
                 '<lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px"></lord-icon>' +
                 '<div class="mt-4 pt-2 fs-15">' +
                 '<h4>Well done !</h4>' +
-                '<p class="text-muted mx-4 mb-0">Aww yeah, you successfully read this important message.</p>' +
+                '<p class="text-muted mx-4 mb-0">{{ $message}}</p>' +
                 '</div>' +
                 '</div>',
             showCancelButton: true,
