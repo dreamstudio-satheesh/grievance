69c1e32edd54763621ce275f92d5b11af11e098e
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-08-10 00:03:03 +0530

    flash message

--- resources/views/layouts/flash-message.blade.php
+++ resources/views/layouts/flash-message.blade.php
@@ -1,14 +1,20 @@
 <script>
     @if ($message = Session::get('success'))
     Swal.fire({
-            position: 'top-end',
-            title: 'Good job!',
-            text: '{{ $message }} !',
-            icon: 'success',
+            html: '<div class="mt-3">' +
+                '<lord-icon src="https://cdn.lordicon.com/lupuorrc.json" trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px"></lord-icon>' +
+                '<div class="mt-4 pt-2 fs-15">' +
+                '<h4>Well done !</h4>' +
+                '<p class="text-muted mx-4 mb-0">Aww yeah, you successfully read this important message.</p>' +
+                '</div>' +
+                '</div>',
+            showCancelButton: true,
             showConfirmButton: false,
-            timer: 1500,
+            cancelButtonClass: 'btn btn-primary w-xs mb-1',
+            cancelButtonText: 'Back',
+            buttonsStyling: false,
             showCloseButton: true
-        });  
+        })
 
 
     @endif
