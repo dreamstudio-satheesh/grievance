e476745c140c2569579224239af9ad585664ca83
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-08-10 19:55:20 +0530

    user export

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,74 +1,103 @@
 @extends('layouts.master')
-@section('title') @lang('translation.dashboards') @endsection
+@section('title') User List @endsection
+
+@section('css')
+<!--datatable css-->
+<link href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" rel="stylesheet" type="text/css" />
+<!--datatable responsive css-->
+<link href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css" rel="stylesheet" type="text/css" />
+@endsection
+
 @section('content')
 @component('components.breadcrumb')
 @slot('li_1') Pages @endslot
 @slot('title') Users  @endslot
 @endcomponent
 
-
-<div class="row g-4 mb-3">
-    <div class="col-sm-auto">
-        <div class="search-box ms-2">
-            <input type="text" class="form-control search" placeholder="Search...">
-            <i class="ri-search-line search-icon"></i>
-        </div>
-        
-    </div>
-    <div class="col-sm">
-        <div class="d-flex justify-content-sm-end">
-            <div>
-                <a href="{{ url('/admin/users/create') }}" class="btn btn-primary add-btn"><i class="ri-add-line align-bottom me-1"></i> Add </a>
+<div class="row">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="card-title mb-0">List of Users</h5>
+                <div class="d-flex justify-content-sm-end">
+                    <div>
+                        <a href="{{ url('/admin/users/create') }}" class="btn btn-primary add-btn"><i class="ri-add-line align-bottom me-1"></i> Add </a>
+                    </div>
+                  
+                </div>
             </div>
-          
+            <div class="card-body">
+                <table id="buttons-datatables" class="display table table-bordered" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Mobile</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>                   
+                     <tbody>
+                            @foreach ($users as $user)
+                        <tr>
+                        <th scope="row"><a href="#" class="fw-semibold">#{{ $user->id }}</a></th>
+                        <td>{{ $user->name }}</td>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->mobile_number }}</td>
+                        <td>
+                          
+                            <div class="d-flex gap-2">
+                                <div class="edit">
+                                    <a href="{{ route('users.edit',$user->id) }}" class="btn btn-sm btn-success edit-item-btn">Edit</a>
+                                </div>
+                                <div class="remove"> 
+                                    <form action="{{ route('users.destroy',$user->id) }}" method="POST"> 
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-sm btn-primary remove-item-btn">Remove</button>
+                                    </form> 
+                                </div>
+                            </div>
+                        </td>
+                        
+                        </tr>
+                             @endforeach
+                       
+                        </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </div>
 
-<table class="table table-nowrap">
-    <thead>
-    <tr>
-    <th scope="col">ID</th>
-    <th scope="col">name</th>
-    <th scope="col">email</th>
-    <th scope="col">mobile number</th>
-    <th scope="col">Action</th>
-    </tr>
-    </thead>
-    <tbody>
-        @foreach ($users as $user)
-    <tr>
-    <th scope="row"><a href="#" class="fw-semibold">#{{ $user->id }}</a></th>
-    <td>{{ $user->name }}</td>
-    <td>{{ $user->email }}</td>
-    <td>{{ $user->mobile_number }}</td>
-    <td>
-      
-        <div class="d-flex gap-2">
-            <div class="edit">
-                <a href="{{ route('users.edit',$user->id) }}" class="btn btn-sm btn-success edit-item-btn">Edit</a>
-            </div>
-            <div class="remove"> 
-                <form action="{{ route('users.destroy',$user->id) }}" method="POST"> 
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-sm btn-primary remove-item-btn">Remove</button>
-                </form> 
-            </div>
-        </div>
-    </td>
-    
-    </tr>
-         @endforeach
-   
-    </tbody>
-    </table>
-
-    
-
-
 @endsection
 @section('script')
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.2.2/js/buttons.html5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+
+<script>
+  document.addEventListener('DOMContentLoaded', function () {
+  let table = new DataTable('#buttons-datatables', {
+        dom: 'Bfrtip',
+        buttons: [
+            'copy', 'csv', 'excel', 'print', 'pdf'
+        ]
+        
+    });
+}); 
+</script>
+
 <script src="{{ URL::asset('/admin_assets/js/app.min.js') }}"></script>
 
 @endsection
